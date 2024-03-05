03aff7054ef887901d9f0d07530ba23970a98f1d
Movs01 <129988676+Movs01@users.noreply.github.com>
2024-03-05 18:09:24 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,9 +1,17 @@
-import random
+# импортируем библиотеку random для работы с рандомными числами
+import random 
 
+#Функция для генерации списка случайных целых чисел в заданном диапазоне.
 def generateList(elementsAmount):
-    list = []
-    for i in range(elementsAmount):
-        list.append(random.randint(5,1000))
-    return list
+    try:
+        if not isinstance(elementsAmount, int) or elementsAmount <= 0:
+            raise ValueError("elementsAmount должен быть положительным целым числом")
+
+        list = []
+        for i in range(elementsAmount):
+            list.append(random.randint(5, 1000))
+        return list
+    except ValueError as e:
+        print("Ошибка:", e)
 
 print(generateList(20))
